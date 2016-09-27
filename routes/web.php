3890cdb24c696029159c3434e42b1b34643ef260
David Martinez <davidmgilo@gmail.com>
2016-09-27 21:04:50 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,11 @@ Route::get('/home', function (){
     return view('home');
 });
 
-Route::get('/login', function (){
-    return view('auth.login');
-});
+//Route::get('/login', function (){
+//    return view('auth.login');
+//});
+
+Route::get('/login', 'LoginController@login');
 
 Route::get('/register', function (){
     return view('auth.register');
