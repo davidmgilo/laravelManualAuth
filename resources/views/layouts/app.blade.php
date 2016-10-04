f628ad30cb3ffe4258ec222426d2b2225653208b
David Martinez <davidmgilo@gmail.com>
2016-10-04 20:59:29 +0200

    layout 2 footer+sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
 @endsection
 
 <body>
+@section('sidebar')
+    @include('layouts.partials.sidebar')
+@endsection
+
+
 @yield('main-content')
+
+@section('footer')
+    @include('layouts.partials.footer')
+@endsection
 </body>
 </html>
