6fa378082bffe973c570bcfc4eb0cebc756c085f
David Martinez <davidmgilo@gmail.com>
2016-10-11 20:24:40 +0200

    query string 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,6 +30,7 @@ class HomeController extends Controller
 
     private function userIsAuthenticated()
     {
+        dd($_GET['user']);
         if (existeuseringet){
             return true;
         }else{
