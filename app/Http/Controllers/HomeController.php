73d9b9f31b97f160aa7cf48e8e0edbda305148e7
David Martinez <davidmgilo@gmail.com>
2016-10-11 20:47:14 +0200

    query string 7

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
             return redirect('login');
         }
 
-      //  '{"name" : "David"}'
+      //  '{"name" : "David", "sn1" : "Martinez"}'
 
     }
 
