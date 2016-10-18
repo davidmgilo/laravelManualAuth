fac4c7fd8be575f79f1960dd9586eb7473cf12f0
David Martinez <davidmgilo@gmail.com>
2016-10-18 20:26:28 +0200

    btn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
 
     Usuari: <input type="text" name="user" id="">
     Password: <input type="text" name="password" id="">
-    <input type="submit" value="Login" class="btn">
+    <input type="submit" value="Login" class="btn btn-primary">
 </form>
 
 </body>
