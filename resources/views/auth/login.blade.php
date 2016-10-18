2343aeeed005a0e965c9a0ee3ee19fa964113bb0
David Martinez <davidmgilo@gmail.com>
2016-10-18 20:24:03 +0200

    bootstrap + btn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta name="csrf-token" content="    {{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
@@ -14,7 +15,7 @@
 
     Usuari: <input type="text" name="user" id="">
     Password: <input type="text" name="password" id="">
-    <input type="submit" value="Login">
+    <input type="submit" value="Login" class="btn">
 </form>
 
 </body>
