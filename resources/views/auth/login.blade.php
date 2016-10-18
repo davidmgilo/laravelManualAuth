6b4c56b7ef395b82f44e48a4f84950c5a879d91f
David Martinez <davidmgilo@gmail.com>
2016-10-18 20:19:12 +0200

    login form token for csrf millor forma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <meta name="csrf-token" content="    {{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -10,7 +11,7 @@
 <body>
 
 <form action="/login" method="post">
-    <input type="hidden" name="token" value="{{csrf_token()}}">
+
     Usuari: <input type="text" name="user" id="">
     Password: <input type="text" name="password" id="">
     <input type="submit" value="Login">
