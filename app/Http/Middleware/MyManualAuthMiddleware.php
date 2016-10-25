b7b2b96474d0fb75256ccd73454bc54d96e7343e
David Martinez <davidmgilo@gmail.com>
2016-10-25 20:10:36 +0200

    manualguard

--- app/Http/Middleware/MyManualAuthMiddleware.php
+++ app/Http/Middleware/MyManualAuthMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\ManualAuth\ManualGuard;
 
 class MyManualAuthMiddleware
 {
---
+++ app/ManualAuth/ManualGuard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\ManualAuth;
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 25/10/16
+ * Time: 20:08
+ */
+class ManualGuard
+{
+
+    /**
+     * ManualGuard constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function check()
+    {
+        
+    }
+}
