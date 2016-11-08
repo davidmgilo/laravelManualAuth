9860bb3deb1d624008c5cfd58654108551716980
David Martinez <davidmgilo@gmail.com>
2016-11-08 20:29:36 +0100

    sync

---
+++ app/ManualAuth/UserProviders/EloquentUserProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\ManualAuth\UserProviders;
+
+
+use App\User;
+use Hash;
+use Session;
+
+class EloquentUserProvider implements UserProvider
+{
+
+    public function validate(array $credentials)
+    {
+        try{
+            $user = User::where(["email" => $credentials['email'] ])->firstOrFail();
+        }catch (\Exception $e){
+            Session::flash('error',["Usuari no trobat a la base de dades"]);
+            return redirect('login');
+        }
+
+        if(Hash::check($credentials['password'],$user->password) ){
+            return redirect('home');
+        }else {
+            return redirect('login');
+        }
+    }
+}
