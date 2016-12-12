5549cd0d6c409c5b931a901c9e7bc2f093108c8f
David Martinez <davidmgilo@gmail.com>
2016-12-12 16:53:40 +0100

    modified root url to go to home && register button on login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,6 +37,11 @@
         </form>
     </div>
     <div class="col-md-4"></div>
+    <form action="/register" method="get">
+        <div class="row text-center">
+            <input type="submit" value="Register" class="btn btn-primary">
+        </div>
+    </form>
 
 </div>
 
